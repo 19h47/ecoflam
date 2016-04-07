48d14d19bfc72510d24ce46b9da00de02cd57af5
Levron Jérémy <levronjeremy@19h47.fr>
2016-04-08 00:19:28 +0200

    JS

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -978,9 +978,9 @@ var app = {
 
                     $($container).fadeOut();
                     
+                    image.close();
                     // Ensure menu is closed
                     menu.close();
-                    image.close();
                 }
             },
             onProgress: {
@@ -1009,12 +1009,13 @@ var app = {
             onAfter: function($container, $newContent){
                 // Reload script
                 _this._reload();
-                if( $('div.wpcf7 > form').length ){
-                    console.log('wpcf7 selector exist');
-                    // $('div.wpcf7 > form').wpcf7InitForm();
-                } else {
-                    console.log('wpcf7 selector doesn\'t exist');
-                }
+                
+                // if( $('div.wpcf7 > form').length ){
+                //     console.log('wpcf7 selector exist');
+                //     // $('div.wpcf7 > form').wpcf7InitForm();
+                // } else {
+                //     console.log('wpcf7 selector doesn\'t exist');
+                // }
 
                 $($container).fadeIn();
 
@@ -1305,9 +1306,14 @@ var page = {
             }, 'xml');
 
         });
-    }
-	
+    }	
 };
+
+$( function() {
+
+    page.init();
+
+});
 var menu = {
 
 	_classMenuOpen: 'menu--is-open',
@@ -1350,7 +1356,7 @@ var menu = {
                 // console.log( e.target );
 
 	            if (_this._$body.hasClass( _this._classMenuOpen) && !$(e.target).closest('.site-header__nav').length) {
-	                e.preventDefault();
+	                // e.preventDefault();
 	                _this.close();
 	        	}
 	    });
